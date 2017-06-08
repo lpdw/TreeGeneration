5496836cfc5f3861829635c300eff4aae98c0913
Sébastien <sebastien@MC-G-LP-C120-9.local>
2017-06-08 13:33:56 +0200

    usable branche

--- tree_setup.js
+++ tree_setup.js
@@ -16,19 +16,31 @@ oCanvas.domReady(function () {
     tree.redraw();
   };
   var title = tree.display.text({size: 40, x:HTMLcanvas.width/2,origin:{x:"center"},fill: "#222", text:"MotherF***ing Tree"}).add();
-  console.log(title);
 
-  var points = [{x:20,y:30}, {x:30,y:50}];
 
-  console.log(points);
+  var points = [{x:HTMLcanvas.width/2,y:0}, {x:HTMLcanvas.width/2 + 2,y:50}, {x:HTMLcanvas.width/2 - 4,y:100}, {x:HTMLcanvas.width/2 - 10,y:150}, {x:HTMLcanvas.width/2 + 1,y:320}];
+  var sous_points = [{x:0,y:0}, {x:-10,y:0}, {x:-35,y:0}, {x:-40,y:20}, {x:-50,y:15}];
 
-  var leaf = tree.display.leaf({points: points, animationStade:0}).add();
-  leaf.animate({
-		animationStade: 100
-	}, {
-		easing: "ease"
-	});
-  console.log(leaf.points);
+
+  var branche = tree.display.branche({
+                            points: points,
+                            strokeWidth: 10,
+                            strokeColor: "green",
+                            animationStade: 100}).add();
+  var sousbranche = tree.display.branche({
+    strokeWidth:7,
+    strokeColor:"green",
+    points: sous_points,
+    startPoint: 90,
+    animationStade: 100
+  });
+  branche.addChild(sousbranche);
+  // sousbranche.animate({
+	// 	animationStade: 100
+	// }, {
+	// 	easing: "ease"
+	// });
+  console.log(sousbranche);
 
   title.dragAndDrop();
  /*TODO: Socket.io : recevoir nouvelles données du serveur
