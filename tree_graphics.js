cceda2ffc4f188a3f8d19e91b7509aef37f0024d
Sébastien <sebastien@MC-G-LP-C120-9.local>
2017-06-08 10:01:23 +0200

    starting animation

--- tree_graphics.js
+++ tree_graphics.js
@@ -1,42 +1,80 @@
+function sleep(miliseconds) {
+   var currentTime = new Date().getTime();
+
+   while (currentTime + miliseconds >= new Date().getTime()) {
+   }
+}
+
 /******************************************** Extend exemple ***************************************/
 /* TODO: Objets de base Leaf et Branche */
-var orbite = function (settings, core){
+var leaf = function (settings, core){
   return oCanvas.extend({
     core: core,
-    shapeType: "radial",
-    minorAxis:0,
-    omega:0,
-    offset:{x:0,y:0},
+    // shapeType: "radial",
+    // minorAxis:0,
+    // omega:0,
+    // offset:{x:0,y:0},
+    animationStade:0,
+    points:[],
     init: function(){
+      console.log("hello init - 1");
+      var canvas = this.core.canvas;
+
+      // canvas.beginPath();
+      // canvas.lineWidth = 10;
+      // canvas.strokeStyle = 'green';
+      // for(var i = 0 ; i<7 ; i++){
+      //     canvas.lineTo(50 + (50 *  (Math.cos(i) + 1) ), 50 + (50 * (Math.sin(i) + 1)) );
+      //     sleep(200);
+      //     console.log("hello init: " + i);
+      //     canvas.redraw();
+      // }
+      // canvas.stroke();
+      // canvas.closePath();
+
     },
     draw: function(){
+      console.log("hello draw");
+      console.log(this.animationStade);
       var canvas = this.core.canvas,
         origin = this.getOrigin();
-        majorAxis = this.majorAxis,
-        inclinaison = this.inclinaison,
-        eccentricity = this.eccentricity,
-        omega = Math.radians(this.omega),
-        excDist = majorAxis - ( ( 1 - eccentricity ) * majorAxis);
-        // Mm = Math.sqrt(Math.pow(majorAxis,3)/GM),
-        this.offset = {
-          x: excDist * Math.cos(omega),
-          y: excDist * Math.sin(omega)
-        };
-        this.minorAxis = Math.sqrt((1 - Math.pow(eccentricity,2)) * Math.pow(majorAxis, 2));
-
-      canvas.beginPath();
-			if (this.strokeWidth > 0) {
-				canvas.strokeStyle = this.strokeColor;
-				canvas.lineWidth = this.strokeWidth;
-        // for(var i = 0; i < 200; i++){
-        //    var point = calculPosition(Math.radians(i), majorAxis, 90, eccentricity, Mm );
-        //    canvas.lineTo(point.x, point.y);
-        // }
-        canvas.ellipse(origin.x + this.offset.x, origin.y + this.offset.y, majorAxis, this.minorAxis, omega, 0, 2 * Math.PI);
-			}
-      canvas.stroke();
-			canvas.closePath();
+        points = this.points;
+        console.log(points);
+        canvas.beginPath();
+        canvas.lineWidth = 20;
+        if(points == null )
+          return
+        var length = points.length * (this.animationStade / 100);
+        for(var i = 0; i < length; i++){
+          canvas.lineTo(points[i].x, points[i].y);
+        }
+        canvas.stroke();
+        canvas.closePath();
+      //   majorAxis = this.majorAxis,
+      //   inclinaison = this.inclinaison,
+      //   eccentricity = this.eccentricity,
+      //   omega = Math.radians(this.omega),
+      //   excDist = majorAxis - ( ( 1 - eccentricity ) * majorAxis);
+      //   // Mm = Math.sqrt(Math.pow(majorAxis,3)/GM),
+      //   this.offset = {
+      //     x: excDist * Math.cos(omega),
+      //     y: excDist * Math.sin(omega)
+      //   };
+      //   this.minorAxis = Math.sqrt((1 - Math.pow(eccentricity,2)) * Math.pow(majorAxis, 2));
+      //
+      // canvas.beginPath();
+			// if (this.strokeWidth > 0) {
+			// 	canvas.strokeStyle = this.strokeColor;
+			// 	canvas.lineWidth = this.strokeWidth;
+      //   // for(var i = 0; i < 200; i++){
+      //   //    var point = calculPosition(Math.radians(i), majorAxis, 90, eccentricity, Mm );
+      //   //    canvas.lineTo(point.x, point.y);
+      //   // }
+      //   canvas.ellipse(origin.x + this.offset.x, origin.y + this.offset.y, majorAxis, this.minorAxis, omega, 0, 2 * Math.PI);
+			// }
+      // canvas.stroke();
+			// canvas.closePath();
     }
   },settings);
 };
-oCanvas.registerDisplayObject("orbite", orbite, "init");
+oCanvas.registerDisplayObject("leaf", leaf, "init");
--- tree_setup.js
+++ tree_setup.js
@@ -12,6 +12,16 @@ oCanvas.domReady(function () {
     tree.redraw();
   };
   var title = tree.display.text({size: 40, x:HTMLcanvas.width/2,origin:{x:"center"},fill: "#222", text:"MotherF***ing Tree"}).add();
+  console.log(title);
+  var points = [{x:10, y:20},{x:100,y:200},{x:300,y:400},{x:350,y:400},{x:400,y:400},{x:500,y:500},{x:600,y:800}];
+  console.log(points);
+  var leaf = tree.display.leaf({points: points, animationStade:0}).add();
+  leaf.animate({
+		animationStade: 100
+	}, {
+		easing: "ease"
+	});
+  title.dragAndDrop();
  /*TODO: Socket.io : recevoir nouvelles données du serveur
  Faire touner l'algo de génération avec les nouvelles données*/
 });
