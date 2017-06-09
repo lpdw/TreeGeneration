52a499e391737330e67107d14d4f93a5ef67f87e
msainjon <lpcm@MC-G-LP-C120-18.local>
2017-06-09 16:02:20 +0200

    generation arbre

--- assets/js/tree_generator.js
+++ assets/js/tree_generator.js
@@ -2,16 +2,20 @@ var point_input= 0;
 var list_input = [1,1,1,1,1,1,1];
 var tree_global;
 
+var BranchePrincipal = [];
 var BrancheSecondaire = [];
 
-var branchetypeA = [{x:0,y:0}, {x:50,y:0}, {x:100,y:-10}, {x:150,y:10}, {x:200,y:15}, {x:250,y:-20}];
+var branchetypeA = [{x:0,y:0}, {x:50,y:0}, {x:100,y:-10}, {x:150,y:10}, {x:200,y:15}, {x:250,y:-0}];
 var branchetypeB = [{x:0,y:0}, {x:-50,y:0}, {x:-100,y:10}, {x:-150,y:30}, {x:-200,y:25}, {x:-250,y:30}, {x:-300,y:40}];
 var branchetypeC = [{x:0,y:0}, {x:50,y:30}, {x:100,y:50}, {x:150, y:40}, {x:200,y:60}, {x:250,y:80}, {x:300,y:80}];
 var branchetypeD = [{x:0,y:0}, {x:-50,y:25}, {x:-75,y:30}, {x:-80,y:40}, {x:-90,y:60}, {x:-100,y:80}, {x:-150,y:100}, {x:-200,y:150}];
 var branchetypeE = [{x:0,y:0}, {x:5,y:-5}, {x:5,y:20}, {x:-5,y:30}, {x:5,y:50}, {x:-5,y:100}, {x:5,y:250}];
 
 var SousbranchetypeA = [{x:0,y:0}, {x:10,y:10}, {x:35,y:20}, {x:40,y:30}, {x:70,y:45}];
+var SousbranchetypeB = [{x:0,y:0}, {x:10,y:-10}, {x:35,y:-20}, {x:40,y:-30}, {x:70,y:-45}];
 
+var i =0;
+var position = 85;
 
 var algo = function(tree) {
   tree_global = tree;
@@ -19,17 +23,74 @@ var algo = function(tree) {
     generate: function(datas){
       point_input = traitementInput(datas);
 
-      //SWITCH
-      var branche_principal_A = initBranche(tree_global, branchetypeA, 89);
-      var branche_principal_B = initBranche(tree_global, branchetypeB, 80);
-      var branche_principal_C = initBranche(tree_global, branchetypeC, 90);
-      var branche_principal_D = initBranche(tree_global, branchetypeD, 85);
-      var branche_principal_E = initBranche(tree_global, branchetypeE, 95);
+      if(BranchePrincipal.find(trouverBranche("branche_principal_A"))){
+        var branche_principal_A = BranchePrincipal.find(trouverBranche("branche_principal_A")).branche;
+      }
+      else{
+        var branche_principal_A = initBranche(tree_global, branchetypeA, 89);
+        BranchePrincipal.push({name:"branche_principal_A", branche:branche_principal_A});
+      }
 
-      initSecondaryBranch(tree_global, SousbranchetypeA, 85, "branche_principal_A", branche_principal_A);
+
+      if(BranchePrincipal.find(trouverBranche("branche_principal_B"))){
+        var branche_principal_B = BranchePrincipal.find(trouverBranche("branche_principal_B")).branche;
+      }
+      else
+      {
+        var branche_principal_B = initBranche(tree_global, branchetypeB, 80);
+        BranchePrincipal.push({name:"branche_principal_B", branche:branche_principal_B});
+      }
+
+      if(BranchePrincipal.find(trouverBranche("branche_principal_C"))){
+        var branche_principal_C = BranchePrincipal.find(trouverBranche("branche_principal_C")).branche;
+      }
+      else
+      {
+        var branche_principal_C = initBranche(tree_global, branchetypeC, 90);
+        BranchePrincipal.push({name:"branche_principal_C", branche:branche_principal_C});
+      }
+
+      if(BranchePrincipal.find(trouverBranche("branche_principal_D"))){
+        var branche_principal_D = BranchePrincipal.find(trouverBranche("branche_principal_D")).branche;
+      }
+      else
+      {
+        var branche_principal_D = initBranche(tree_global, branchetypeD, 85);
+        BranchePrincipal.push({name:"branche_principal_D", branche:branche_principal_D});
+      }
+
+      if(BranchePrincipal.find(trouverBranche("branche_principal_E"))){
+        var branche_principal_E = BranchePrincipal.find(trouverBranche("branche_principal_E")).branche;
+      }
+      else
+      {
+        var branche_principal_E = initBranche(tree_global, branchetypeE, 88);
+        BranchePrincipal.push({name:"branche_principal_E", branche:branche_principal_E});
+      }
+
+      if(i==0){
+        BrancheSecondaire.push(initSecondaryBranch(tree_global, SousbranchetypeA, position, "branche_principal_A", branche_principal_A));
+        position=position-20;
+        i++;
+      }
+      else{
+        BrancheSecondaire.push(initSecondaryBranch(tree_global, SousbranchetypeB, 50, "branche_principal_A", branche_principal_A));
+        position=position-20;
+        i--;
+      }
     },
   }
 }
+
+function trouverBranche(name) {
+    return function(element) {
+      if(element.name == name) {
+        return element;
+      }
+    }
+}
+
+
 
 function initBranche(tree_global, branchetype, InitialPoint){
   var branche =  tree_global.display.branche({
@@ -55,8 +116,6 @@ function initSecondaryBranch(tree_global, brancheType, InitialPoint, parent_name
     });
 
     parent.addChild(sub_branch);
-
-    console.log(sub_branch);
     return sub_branch;
 
 }
