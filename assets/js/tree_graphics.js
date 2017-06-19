f8adafbfa3d7f2752a99e5b8f6ef50419f865895
sebberrou <seb.berrou@laposte.net>
2017-06-20 01:21:42 +0200

    responsive Point, relative point array on init

--- assets/js/tree_graphics.js
+++ assets/js/tree_graphics.js
@@ -10,26 +10,36 @@ function sleep(miliseconds) {
    while (currentTime + miliseconds >= new Date().getTime()) {
    }
 }
-function trueY(y){
-  return document.getElementById("tree").height - y;
-}
-
 function drawPoly(ctx, start, sides, angle, size){
-  var center = {x: start.x + (size/2 * Math.cos(angle)), y: start.y + (size/2 * Math.sin(angle)) };
+  var center = new Point(start.x + (size/2 * Math.cos(angle)), start.y + (size/2 * Math.sin(angle)) );
   var a = (Math.PI * 2) / sides;
-  ctx.moveTo(center.x + (size * Math.cos(angle + Math.PI * 2)), trueY(center.y + ( size * Math.sin(angle + Math.PI * 2))));
+  ctx.moveTo(center.drawX(size * Math.cos(angle + Math.PI * 2)), center.drawY(size * Math.sin(angle + Math.PI * 2)));
 
   for (var i = 1; i <= sides; i++) {
-    ctx.lineTo( center.x + ( size * Math.cos(angle + a*i)), trueY(center.y + ( size * Math.sin(angle + a*i))));
+    ctx.lineTo( center.drawX(size * Math.cos(angle + a*i)),center.drawY( size * Math.sin(angle + a*i)));
   }
 }
 function drawArrow(ctx, start, end, headlen){
   var angle = Math.atan2(start.y-end.y,start.x-end.x);
-  ctx.moveTo(end.x-headlen*Math.cos(angle-Math.PI/7),trueY(end.y-headlen*Math.sin(angle-Math.PI/7)));
-  ctx.lineTo(end.x, trueY(end.y));
-  ctx.lineTo(end.x-headlen*Math.cos(angle+Math.PI/7),trueY(end.y-headlen*Math.sin(angle+Math.PI/7)));
+  ctx.moveTo(end.drawX(headlen*Math.cos(angle-Math.PI/7)),end.drawY(headlen*Math.sin(angle-Math.PI/7)));
+  ctx.lineTo(end.drawX(), end.drawY());
+  ctx.lineTo(end.drawX(-headlen*Math.cos(angle+Math.PI/7)),end.drawY(-headlen*Math.sin(angle+Math.PI/7)));
 }
-
+// Objet point à utiliser, méthodes drawX et drawY déssinent en pour 1000 sur canvas
+function Point(x = 0, y = 0){
+  this.x = x;
+  this.y = y;
+}
+Point.prototype.drawX = function(xoffset = 0){
+  return ( (this.x + xoffset) / 100) * document.getElementById("tree").width;
+}
+Point.prototype.drawY = function(yoffset = 0){
+  return document.getElementById("tree").height - ((this.y + yoffset)  / 100 * document.getElementById("tree").height);
+}
+Point.prototype.set = function(point){
+  this.x = point.x;
+  this.y = point.y;
+}
 var cyan = 'hsl(187, 47%, 55%)',
 blue = 'hsl(207, 82%, 66%)',
 purple = 'hsl(286, 90%, 47%)',
@@ -37,7 +47,7 @@ green = 'hsl(95, 68%, 62%)',
 red1 = 'hsl(355, 75%, 55%)',
 red2 = 'hsl(5, 48%, 51%)',
 orange1 = 'hsl(29, 84%, 61%)',
-orange2 = 'hsl(39, 67%, 69%)',
+orange2 = 'hsl(39, 90%, 69%)',
 black = 'hsl(220,13%,18%)';
 /* Parametres :
 Feuilles : origin,
@@ -68,12 +78,11 @@ var leaf = function (settings, core){
     draw: function(){
       var canvas = this.core.canvas,
         origin = this.getOrigin(),
-        size = this.size,
+        size = this.size/2,
         angle = Math.radians(this.angle);
 
 
         if (this.parent.points != undefined && settings.startPoint && !this.isInit){
-
           this.originPoint = this.parent.points[Math.round((settings.startPoint) / 100 * (this.parent.points.length - 1 ) )];
           this.isInit = true;
         }
@@ -83,17 +92,17 @@ var leaf = function (settings, core){
         canvas.fillStyle = this.strokeColor;
         canvas.lineJoin = this.join;
         canvas.lineCap = this.cap;
-
-        canvas.moveTo(this.originPoint.x, trueY(this.originPoint.y));
-        var end_line = {x: this.originPoint.x + (size * 2 * Math.cos(angle)), y: this.originPoint.y + (size * 2 * Math.sin(angle)) };
-        canvas.lineTo(end_line.x, trueY(end_line.y));
+        var origin = new Point(this.originPoint.x,this.originPoint.y );
+        canvas.moveTo(origin.drawX(), origin.drawY());
+        var end_line = new Point(this.originPoint.x + (size * Math.cos(angle)),this.originPoint.y + (size * Math.sin(angle))) ;
+        canvas.lineTo(end_line.drawX(), end_line.drawY());
         canvas.stroke();
         canvas.closePath();
         canvas.beginPath();
 
         if(this.shape === "square"){
             //
-            var center = {x: end_line.x + (size/2 * Math.cos(angle)), y: end_line.y + (size/2 * Math.sin(angle)) };
+            var center = new Point(end_line.x + (size/2 * Math.cos(angle)),end_line.y + (size/2 * Math.sin(angle)) );
             //canvas.rect(center.x- (size), trueY(center.y)- (size), size*2, size*2);
 
 
@@ -115,8 +124,9 @@ var leaf = function (settings, core){
         }
         else if(this.shape === "circle") {
           // size = size/2;
-          var center = {x: end_line.x + (size * Math.cos(angle)), y: end_line.y + (size * Math.sin(angle)) };
-          canvas.arc(center.x, trueY(center.y), size, 0, Math.PI * 2, false);
+
+          var center = new Point(end_line.x + (size / 3 * Math.cos(angle)), end_line.y + (size / 3 * Math.sin(angle)) );
+          canvas.arc(center.drawX(), center.drawY(), size * 2, 0, Math.PI * 2, false);
           if(this.shapeFill){
             canvas.stroke();
             canvas.closePath();
@@ -124,7 +134,7 @@ var leaf = function (settings, core){
 
             canvas.lineWidth = 0;
 
-            canvas.arc(center.x, trueY(center.y), size/4, 0, Math.PI * 2, false);
+            canvas.arc(center.drawX(), center.drawY(), size/4, 0, Math.PI * 2, false);
             canvas.fill();
           }
 
@@ -164,6 +174,12 @@ var branche = function (settings, core){
     join: "square",
     originPoint:undefined,
     init: function(){
+      if(settings.points != undefined){
+        for(var i = 1;i<settings.points.length;i++){
+          settings.points[i].x = settings.points[i-1].x + settings.points[i].x;
+          settings.points[i].y = settings.points[i-1].y + settings.points[i].y;
+        }
+      }
     },
     draw: function(){
       var canvas = this.core.canvas;
@@ -179,7 +195,6 @@ var branche = function (settings, core){
           this.points = settings.points.map(function(point){
             return {x: (origin.x + point.x), y: (origin.y + point.y)};
           });
-
           this.isInit = true;
         }
 
@@ -187,23 +202,28 @@ var branche = function (settings, core){
           return;
         points = this.points;
         var progress = this.animationStade / 100;
-        var lastpoint = points[points.length - 1];
+        var lastpoint = new Point(points[points.length - 1].x,points[points.length - 1].y) ;
+        var currentPoint = new Point();
+        var prevPoint = new Point();
         for(var i = 0; i < points.length; i++){
+
           if(i == points.length - 1 && progress != 1){
-            // Anime la dernière partie du chemin, selon le paramètre animationStade
-            lastpoint = {x: points[i - 1].x + ((points[i].x - points[i - 1].x) * progress) ,
-                        y: points[i - 1].y + ((points[i].y - points[i - 1].y) * progress)}
-            canvas.lineTo(lastpoint.x,trueY(lastpoint.y));
+            // Anime la dernière partie du chemin, selon le paramètre progress
+            lastpoint.x =  points[i - 1].x + ((points[i].x - points[i - 1].x) * progress);
+            lastpoint.y =  points[i - 1].y + ((points[i].y - points[i - 1].y) * progress);
+            canvas.lineTo(lastpoint.drawX(),lastpoint.drawY());
             continue;
           }
-          if(i%5 == 0 && i!=0){
+          currentPoint.set(points[i]);
+          if(i%20 == 0 && i!=0){
             canvas.stroke();
             canvas.closePath();
             canvas.beginPath();
             canvas.lineWidth = canvas.lineWidth * 0.7;
-            canvas.moveTo(points[i-1].x, trueY(points[i-1].y))
+            prevPoint.set(points[i-1]);
+            canvas.moveTo(prevPoint.drawX(),prevPoint.drawY());
           }
-          canvas.lineTo(points[i].x, trueY(points[i].y));
+          canvas.lineTo(currentPoint.drawX(), currentPoint.drawY());
         }
         canvas.stroke();
         canvas.closePath();
@@ -215,32 +235,32 @@ var branche = function (settings, core){
           var invProgress = 1 - progress;
           //progress = progress == 0 ? 0.0001 : progress;
           var max = Math.floor(280 * invProgress);
-          var first_point;
+          var first_point = new Point();
           var start = max > 40 ? max - 40 : 0;
           canvas.beginPath();
           for (i = start; i<= max; i++) {
             var angle =  0.1 * Math.pow(i,(1 - progress));
             //var angle =  Math.pow(i,i / 100);
 
-             x=lastpoint.x + (1*i+angle + ( invProgress * 5))*Math.cos(angle);
-             y=lastpoint.y + (1*i+angle + ( invProgress * 5))*Math.sin(angle);
+             currentPoint.x=lastpoint.x + (1*i+angle + ( invProgress * 5))*Math.cos(angle);
+             currentPoint.y=lastpoint.y + (1*i+angle + ( invProgress * 5))*Math.sin(angle);
             if(i == start ){
                 //canvas.moveTo(x, trueY(y));
-                first_point = {x:x,y:y};
+                first_point = currentPoint;
                 continue;
             }
             else if(i == start+1){
 
-              drawArrow(canvas,first_point,{x:x,y:y},30*invProgress);
+              drawArrow(canvas,first_point,currentPoint,10*invProgress);
               canvas.stroke();
               canvas.closePath();
               canvas.beginPath();
-              canvas.moveTo(x, trueY(y));
+              canvas.moveTo(currentPoint.drawX(), currentPoint.drawY());
               continue;
               // canvas.lineTo(first_point.x,trueY(first_point.y));
               // canvas.lineTo(x,trueY(y));
             }
-            canvas.lineTo(x,trueY(y));
+            canvas.lineTo(currentPoint.drawX(), currentPoint.drawY());
            }
 
            canvas.stroke();
--- assets/js/tree_setup.js
+++ assets/js/tree_setup.js
@@ -1,3 +1,37 @@
+var words = {
+1: "Tristesse",
+2: "Peur",
+3: "Ignorance",
+4: "Joie" ,
+5: "Plaisir",
+6: "Connaissance",
+7: "Toucher",
+8: "Écouter",
+9: "Voir",
+10: "Sentir",
+11: "Création",
+12: "Virtuel",
+13: "Curiosité",
+14: "Entraide",
+15: "Environnement",
+16: "Savoir-faire",
+17: "Nature",
+18: "Ludique",
+19: "Do It Yourself",
+20: "Arts",
+21: "Citoyen",
+22: "Technique",
+23: "Connecté",
+24: "Ludique",
+25: "Sensibilité",
+26: "Expérience",
+27: "Atelier",
+28: "Narration",
+29: "Savoir-être",
+30: "Outil",
+}
+
+
 oCanvas.domReady(function () {
   var HTMLcanvas = document.getElementById("tree");
   var HTMLbutton = document.getElementById("button");
@@ -42,17 +76,32 @@ oCanvas.domReady(function () {
   //var title = tree.display.text({size: 40, x:HTMLcanvas.width/2,origin:{x:"center"},fill: "#222", text:"MotherF***ing Tree"}).add();
 
 
-  var points = [{x:tree.width/2,y:0}, {x:tree.width/2 + 2,y:50},
-    {x:tree.width/2 - 4,y:70}, {x:tree.width/2 + 1,y:110},
-    {x:tree.width/2 + 1,y:140} ,{x:tree.width/2 - 10,y:170},
-        {x:tree.width/2 - 4,y:220}, {x:tree.width/2 + 1,y:250},
-    {x:tree.width/2 + 1,y:280}];
-  var sous_points = [{x:0,y:0}, {x:5,y:0}, {x:7,y:0}, {x:9,y:2}, {x:11,y:2}, {x:15,y:2}, {x:17,y:4}, {x:19,y:4}, {x:21,y:4}, {x:24,y:4}, {x:25,y:4}, {x:28,y:4}];
-  var sous_sous_points = [{x:0,y:0}, {x:10,y:0}, {x:35,y:0}, {x:40,y:20}, {x:70,y:25}];
+  // var points = [{x:tree.width/2,y:0}, {x:tree.width/2 + 2,y:50},
+  //   {x:tree.width/2 - 4,y:70}, {x:tree.width/2 + 1,y:110},
+  //   {x:tree.width/2 + 1,y:140} ,{x:tree.width/2 - 10,y:170},
+  //       {x:tree.width/2 - 4,y:220}, {x:tree.width/2 + 1,y:250},
+  //   {x:tree.width/2 + 1,y:280}];
+    var rel_points = [];
+    var sous_points = [];
+    var sous_sous_points = [];
 
+    for(var i= 0; i<70; i++){
+      rel_points[i] = {x: .5 + Math.cos(i) * 1.1,
+                        y: .5 + Math.cos(i) * 1.1 };
+    }
+    for(var i= 0; i<40; i++){
+      sous_points[i] = {x: 1 + Math.sin(i),
+                        y: 1 + Math.sin(i) };
+    }
+    for(var i= 0; i<30; i++){
+      sous_points[i] = {x: 0.5 + Math.cos(i),
+                        y: .4 + Math.sin(i) };
+    }
+  // var sous_points = [{x:0,y:0}, {x:5,y:0}, {x:7,y:0}, {x:9,y:2}, {x:11,y:2}, {x:15,y:2}, {x:17,y:4}, {x:19,y:4}, {x:21,y:4}, {x:24,y:4}, {x:25,y:4}, {x:28,y:4}];
+  // var sous_sous_points = [{x:0,y:0}, {x:10,y:0}, {x:35,y:0}, {x:40,y:20}, {x:70,y:25}];
 
   var branche = tree.display.branche({
-                            points: points,
+                            points: rel_points,
                             strokeWidth: 15,
                             strokeColor: "white",
                             join: "round",
@@ -69,7 +118,7 @@ oCanvas.domReady(function () {
   branche.addChild(sousbranche);
   var soussousbranche = tree.display.branche({
     strokeWidth:15,
-    strokeColor:"black",
+    strokeColor:orange2,
     points: sous_sous_points,
     startPoint: 100
   });
@@ -107,10 +156,7 @@ oCanvas.domReady(function () {
   });
 
   branche.addChild(nleaftriangle);
-  branche.addPoint({x:0 + 1,y:15});
-  branche.addPoint({x:0 + 1,y:15});
-  branche.addPoint({x:0 + 1,y:15});
-  branche.addPoint({x:0 + 1,y:15});
+  branche.addPoint({x:0 + 1,y:2});
 
   //sousbranche.addChild(soussousbranche);
   //branche.points.push({x:tree.width/2 + 1,y:420})
@@ -124,18 +170,18 @@ oCanvas.domReady(function () {
   //console.log(branche);
   //branche.scalingX = 0.4;
   //branche.scalingY = 0.4;
-  tree.redraw();
+  //tree.redraw();
  // Faire touner l'algo de génération avec les nouvelles données*/
  //  algo.init(tree);
   // algo.generate(tree);
-  var i = 0;
-  tree.setLoop(function () {
-    nleaf.angle = nleaf.angle > 180 ? nleaf.angle + ( (Math.cos(i)- 0.5) * 5) : nleaf.angle - ( (Math.cos(i)- 0.5) * 5);
-    nleafsquare.angle = nleafsquare.angle > 180 ? nleafsquare.angle + ( (Math.cos(i)- 0.5) * 5) : nleafsquare.angle - ( (Math.cos(i)- 0.5) * 5);
-    nleaftriangle.angle = nleaftriangle.angle > 180 ? nleaftriangle.angle + ( (Math.cos(i)- 0.5) * 5) : nleaftriangle.angle - ( (Math.cos(i)- 0.5) * 5);
-    i = i > 10 ? 0 : i +0.01 ;
-  });
+  // var i = 0;
+  // tree.setLoop(function () {
+  //   nleaf.angle = nleaf.angle > 180 ? nleaf.angle + ( (Math.cos(i)- 0.5) * 5) : nleaf.angle - ( (Math.cos(i)- 0.5) * 5);
+  //   nleafsquare.angle = nleafsquare.angle > 180 ? nleafsquare.angle + ( (Math.cos(i)- 0.5) * 5) : nleafsquare.angle - ( (Math.cos(i)- 0.5) * 5);
+  //   nleaftriangle.angle = nleaftriangle.angle > 180 ? nleaftriangle.angle + ( (Math.cos(i)- 0.5) * 5) : nleaftriangle.angle - ( (Math.cos(i)- 0.5) * 5);
+  //   i = i > 10 ? 0 : i +0.01 ;
+  // });
   //tree.timeline.start();
-  var algo_tree = algo(tree);
+  // var algo_tree = algo(tree);
 
 });
