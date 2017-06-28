21305fb43cc9b93c4745e4b3b2b6b3038167571f
ValentinKajdan <valentin.kajdan@orange.fr>
2017-06-28 10:52:44 +0200

    design first animation

--- assets/js/tree_generator.js
+++ assets/js/tree_generator.js
@@ -1,3 +1,48 @@
+function rand(min, max) {
+
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+
+}
+
+(genClips = function() {
+
+  // For easy use
+  $t = $('.startAnimation');
+
+  // Like I said, we're using 5!
+  var amount = 5;
+
+  // Get the width of each clipped rectangle.
+  var width = $t.width() / amount;
+  var height = $t.height() / amount;
+
+  // The total is the square of the amount
+  var totalSquares = Math.pow(amount, 2);
+
+  // The HTML of the content
+  // var html = $t.find('.content').html();
+
+  var y = 0;
+
+  for(var z = 0; z <= (amount*width); z = z+width) {
+// clip-path: circle(60px at center);
+    $('<div class="clipped" style="clip-path: circle(10px at center"></div>').appendTo($t);
+
+    if(z === (amount*width)-width) {
+
+      y = y + height;
+      z = -width;
+
+    }
+
+    if(y === (amount*height)) {
+      z = 9999999;
+    }
+
+  }
+
+})();
+
 var words = {
 Tristesse: 1,
 Peur:2,
@@ -77,11 +122,103 @@ var algo =  {
   startAnimation: function(thisParent){
     $('.startAnimation').css("display", "block");
     setTimeout(function(){
-      $('.startAnimation').css("display", "none");
+
+      // EXPLODE
+
+    			// $('.clipped-box .content').css({'display' : 'none'});
+
+    			// Apply to each clipped-box div.
+    			$('.startAnimation .clipped').each(function() {
+
+    				// So the speed is a random speed between 90m/s and 120m/s. I know that seems like a lot
+    				// But otherwise it seems too slow. That's due to how I handled the timeout.
+    				var v = rand(120, 90),
+    					angle = rand(80, 89), // The angle (the angle of projection) is a random number between 80 and 89 degrees.
+    					theta = (angle * Math.PI) / 180, // Theta is the angle in radians
+    					g = -9.8; // And gravity is -9.8. If you live on another planet feel free to change
+
+    				// $(this) as self
+    				var self = $(this);
+
+    				// time is initially zero, also set some random variables. It's higher than the total time for the projectile motion
+    				// because we want the squares to go off screen.
+    				var t = 0,
+    					z, r, nx, ny,
+    					totalt =  15;
+
+    				// The direction can either be left (1), right (-1) or center (0). This is the horizontal direction.
+    				var negate = [1, -1, 0],
+    					direction = negate[ Math.floor(Math.random() * negate.length) ];
+
+    				// Some random numbers for altering the shapes position
+    				var randDeg = rand(-5, 10),
+    					randScale = rand(0.9, 1.1),
+    					randDeg2 = rand(30, 5);
+
+    				// Because box shadows are a bit laggy (by a bit I mean 'box shadows will not work on individual clipped divs at all')
+    				// we're altering the background colour slightly manually, in order to give the divs differentiation when they are
+    				// hovering around in the air.
+    				var color = $(this).css('backgroundColor').split('rgb(')[1].split(')')[0].split(', '),
+    					colorR = rand(-20, 20),  // You might want to alter these manually if you change the color
+    					colorGB = rand(-20, 20),  // To get the right consistency.
+    					newColor = 'rgb('+(parseFloat(color[0])+colorR)+', '+(parseFloat(color[1])+colorGB)+', '+(parseFloat(color[2])+colorGB)+')';
+
+
+    				// And apply those
+    				$(this).css({
+    					'transform' : 'scale('+randScale+') skew('+randDeg+'deg) rotateZ('+randDeg2+'deg)',
+    					'background' : newColor
+    				});
+
+    				// Set an interval
+    				z = setInterval(function() {
+
+    					// Horizontal speed is constant (no wind resistance on the internet)
+    					var ux = ( Math.cos(theta) * v ) * direction;
+
+    					// Vertical speed decreases as time increases before reaching 0 at its peak
+    					var uy = ( Math.sin(theta) * v ) - ( (-g) * t);
+
+    					// The horizontal position
+    					nx = (ux * t);
+
+    					// s = ut + 0.5at^2
+    					ny = (uy * t) + (0.5 * (g) * Math.pow(t, 2));
+
+    					// Apply the positions
+    					$(self).css({'bottom' : (ny)+'px', 'left' : (nx)+'px'});
+
+    					// Increase the time by 0.10
+    					t = t + 0.10;
+
+    					// If the time is greater than the total time clear the interval
+    					if(t > totalt) {
+
+    						// clicked = false;
+    						// first = true;
+
+
+    						$('.startAnimation').css({'top' : '-1000px', 'transition' : 'none'});
+    						$(self).css({'left' : '0', 'bottom' : '0', 'opacity' : '1', 'transition' : 'none', 'transform' : 'none'});
+
+
+    						// Finally clear the interval
+    						clearInterval(z);
+
+    					}
+
+    				}, 10); // Run this interval every 10ms. Changing this will change the pace of the animation
+
+    			});
+
+
+      // END EXPLODE
+
+      // $('.startAnimation').css("display", "none");
       // Une fois l'animation terminée, on peut initialiser le tronc et les points
       thisParent.initTrunk();
       thisParent.addPoints();
-    }, 2800);
+    }, 3000);
   },
   addPoints: function(){
     var points = [];
--- assets/js/tree_setup.js
+++ assets/js/tree_setup.js
@@ -139,21 +139,21 @@ function randomBranches(){
   algo.init(tree);
 // Regarder si mots sur API et les générer ( peut être mettre un delai)
 
-  $.ajax("https://api-tree.herokuapp.com/inputs/BeforeDate/"+(new Date())).done(function(data){
-    console.log(data.inputs);
-    for (var i=0; i<data.inputs.length; i++){
-      algo.generate(data.inputs[i].words, false, true);
-    }
-    tree.redraw();
-    $("#loader").fadeOut(500);
-  });
-  // $.ajax("https://api-tree.herokuapp.com/inputs/BeforeDate/2017-06-20T15:00:30.021Z").done(function(data){
+  // $.ajax("https://api-tree.herokuapp.com/inputs/BeforeDate/"+(new Date())).done(function(data){
+  //   console.log(data.inputs);
   //   for (var i=0; i<data.inputs.length; i++){
   //     algo.generate(data.inputs[i].words, false, true);
   //   }
   //   tree.redraw();
   //   $("#loader").fadeOut(500);
   // });
+  $.ajax("https://api-tree.herokuapp.com/inputs/BeforeDate/2017-06-20T15:00:30.021Z").done(function(data){
+    for (var i=0; i<data.inputs.length; i++){
+      algo.generate(data.inputs[i].words, false, true);
+    }
+    tree.redraw();
+    $("#loader").fadeOut(500);
+  });
   // une foi générer se connecter à socket pour récupérer les suivants.
   var socket = io.connect('https://api-tree.herokuapp.com');
   socket.on('new_inputs', function (data) {
